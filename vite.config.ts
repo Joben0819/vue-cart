dfdf58cd36dbd72bf3320b51451201b4710f6d62
Joben0819 <neboj08@gmail.com>
2024-10-26 15:46:04 +0800

    updated the code

--- vite.config.ts
+++ vite.config.ts
@@ -3,13 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/vue-cart/',
   plugins: [vue()],
-  build:{
-    outDir: 'build'
-  },
-  server:{
-    port: 3000,
-    open: true
-  },
 })
