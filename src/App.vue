f4b0c9e54806ae1326129707617959f06e14177d
Joben0819 <neboj08@gmail.com>
2024-10-27 14:45:18 +0800

    updated the dist

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,9 @@
 
 <template>
+    <nav>
+      <router-link to="/">Home</router-link>
+      <router-link to="/checkout">Checkout</router-link>
+    </nav>
   <router-view></router-view>
 </template>
 
